8075d270201828f0885c83fef350cc7aa98f11f6
Anthony-science <163058949+Anthony-science@users.noreply.github.com>
2024-04-02 07:45:08 +0100

    Task 7 added

---
+++ string_examples/hello_Bob.py
@@ -0,0 +1,47 @@
+# # # print("Hello\n\"Bob\"")
+
+# # # name = "Peter"
+# # # print(f"Hello, {name}")
+
+# # # number_builder = []
+# # # i = 0
+
+# # # while i <= 50:
+# # #     if i % 2 == 0:
+# # #         number_builder.append(str(i))
+# # #     i += 1
+# # # print(" ".join(number_builder))
+
+# # # pet_list = ["cat", "dog", "hamster", "goldfish", "parrot"]
+# # # print (pet_list[0])
+
+# # # num_list = [1, 4, 5, 6, 2, 8]
+# # # print (num_list[1:2])
+
+# # # import copy
+# # # a = [[1, 2, 3], [4, 5, 6]]
+# # # b = a[:]
+# # # c = copy.deepcopy(a)
+# # # b[0][1] = 10
+# # # c[1][1] = 12
+# # # print(a)
+# # # print(b)
+# # # print(c)
+
+# # num_list = ['1', '5', '8', '14', '25', '31']
+# # new_num_list_ints = [int(element) for element in num_list]
+# # by_two_num_list = [i * 2 for i in new_num_list_ints]
+# # print(by_two_num_list)
+
+# profile_dict = {'name': 'Chris',
+# 'surname': 'Smith',
+# 'age': 28,
+# 'cell': '083 233 3242'
+# }
+# print (profile_dict['surname']) # prints out 'Smith'
+# print (profile_dict.get('cell')) # prints out '083 233 3242'
+# keys = profile_dict.keys()
+# values = profile_dict.values()
+# print(keys)
+# # print(values)
+
